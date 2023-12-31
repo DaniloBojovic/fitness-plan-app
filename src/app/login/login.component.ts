8ab9e32b12e6e2471080533eb715eb9410249824
Danilo <danilobojovicdb@gmail.com>
2023-12-31 10:35:32 +0100

    Change to switchMap()

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './../services/auth.service';
 import { LoginResponse } from '../login-response';
 import { Router } from '@angular/router';
+import { tap, switchMap, catchError, delay, interval, of } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -17,27 +18,46 @@ export class LoginComponent {
   constructor(private authService: AuthService, private router: Router) {}
 
   login() {
-    this.authService.login(this.username, this.password).subscribe(
-      (response: LoginResponse) => {
-        localStorage.setItem('token', response.token);
-        console.log(`RESPONSE : ${response.token}`);
+    // this.authService.login(this.username, this.password).subscribe(
+    //   (response: LoginResponse) => {
+    //     localStorage.setItem('token', response.token);
+    //     console.log(`RESPONSE : ${response.token}`);
 
-        //Fetch the user profile
-        this.authService.getProtectedData().subscribe(
-          (data) => {
-            debugger;
-            console.log(data);
-            this.router.navigate(['/dashboard']);
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-      },
-      (error) => {
-        this.errorMessage = 'Invalid username or password';
-        this.isError = true;
-      }
-    );
+    //     this.authService.getProtectedData().subscribe(
+    //       (data) => {
+    //         debugger;
+    //         console.log(data);
+    //         this.router.navigate(['/dashboard']);
+    //       },
+    //       (error) => {
+    //         console.log(error);
+    //       }
+    //     );
+    //   },
+    //   (error) => {
+    //     this.errorMessage = 'Invalid username or password';
+    //     this.isError = true;
+    //   }
+    // );
+
+    this.authService
+      .login(this.username, this.password)
+      .pipe(
+        switchMap((response: LoginResponse) => {
+          localStorage.setItem('token', response.token);
+          console.log(`RESPONSE : ${response.token}`);
+          return this.authService.getProtectedData();
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (error) => {
+          this.errorMessage = 'Invalid username or password';
+          this.isError = true;
+        },
+      });
   }
 }
