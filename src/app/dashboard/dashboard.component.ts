fa98ec9c50eab6de1bfe0aef0845c8b9c7a33f84
Danilo <danilobojovicdb@gmail.com>
2024-01-01 20:12:59 +0100

    Generate PDF for fitness plans

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../services/auth.service';
 import { FitnessPlanService } from '../services/fitness-plan.service';
 import { FitnessPlan } from '../models/fitness-plan.model';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-dashboard',
@@ -31,5 +32,35 @@ export class DashboardComponent {
 
   logout() {
     this.authService.logout();
+  }
+
+  generatePdf(plan: FitnessPlan) {
+    const doc = new jsPDF();
+    const lineHeight = 7;
+    const pageWidth = doc.internal.pageSize.width;
+    const textMargin = 10;
+    const maxLineWidth = pageWidth - 2 * textMargin;
+
+    const titleLines = doc.splitTextToSize(plan.name, maxLineWidth);
+    const descriptionLines = doc.splitTextToSize(
+      plan.description,
+      maxLineWidth
+    );
+
+    let yPosition = 10;
+
+    titleLines.forEach((line: string | string[]) => {
+      doc.text(line, textMargin, yPosition);
+      yPosition += lineHeight;
+    });
+
+    yPosition += lineHeight;
+
+    descriptionLines.forEach((line: string | string[]) => {
+      doc.text(line, textMargin, yPosition);
+      yPosition += lineHeight;
+    });
+
+    doc.save(`${plan.name}.pdf`);
   }
 }
